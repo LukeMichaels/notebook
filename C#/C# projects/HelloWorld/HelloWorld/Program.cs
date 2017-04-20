994784bab90898c6f55560371564ee19bbca5d8c
Luke Michaels <lmichaels@gmail.com>
2017-04-19 23:49:19 -0700

    new C# notes

---
+++ C#/C# projects/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// A simple program to display the words Hello World
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+		Console.Read();
+        }
+    }
+}
